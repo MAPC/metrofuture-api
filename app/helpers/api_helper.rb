d964d569c06a40959a29a6552655510198cb24e4
Matt Cloyd <cloydster@gmail.com>
2015-06-15 15:11:07 -0400

    Include all request params in the pagination links.

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -14,25 +14,40 @@ module ApiHelper
   end
 
   def first_page
-    send("#{ @klass }_path", "page[number]" => 1, "page[size]" => page_size)
+    send("#{ @klass }_path", params.merge({
+      "page[number]" => 1,
+      "page[size]"   => page_size
+    }))
   end
 
   def previous_page(current_page_num)
     return nil if current_page_num <= 1
-    send("#{ @klass }_path", "page[number]" => current_page_num - 1, "page[size]" => page_size)
+    send("#{ @klass }_path", params.merge({
+      "page[number]" => current_page_num - 1,
+      "page[size]"   => page_size
+    }))
   end
 
   def current_page(current_page_num)
-    send("#{ @klass }_path", "page[number]" => current_page_num, "page[size]" => page_size)
+    send("#{ @klass }_path", params.merge({
+      "page[number]" => current_page_num,
+      "page[size]"   => page_size
+    }))
   end
 
   def next_page(current_page_num, last_page_num)
     return nil if current_page_num >= last_page_num
-    send("#{ @klass }_path", "page[number]" => current_page_num + 1, "page[size]" => page_size)
+    send("#{ @klass }_path", params.merge({
+      "page[number]" => current_page_num + 1,
+      "page[size]"   => page_size
+    }))
   end
 
   def last_page(last_page_num)
-    send("#{ @klass }_path", "page[number]" => last_page_num, "page[size]" => page_size)
+    send("#{ @klass }_path", params.merge({
+      "page[number]" => last_page_num,
+      "page[size]"   => page_size
+    }))
   end
 
   def page_size
