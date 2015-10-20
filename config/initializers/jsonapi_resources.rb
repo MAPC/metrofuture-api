8b13339528f1d6fe589d52913de4aaf10a677756
Matt Cloyd <cloydster@gmail.com>
2015-10-20 13:20:08 -0400

    Change max page size to 351. Would prefer to do this on the resource level, but there doesn't appear to be a way to do so. Remove the counting processor (which was a sample anyway).

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -1,11 +1,7 @@
-require 'jsonapi/counting_active_record_operations_processor'
-
 JSONAPI.configure do |config|
   # built in paginators are :none, :offset, :paged
   config.default_paginator = :paged
 
-  config.default_page_size =  9
-  config.maximum_page_size = 27
-
-  config.operations_processor = :counting_active_record
+  config.default_page_size =   9
+  config.maximum_page_size = 351
 end
